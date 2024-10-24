b675ec77d789209dfb04bc310cf5aba9ad85d792
UnchainedDev <abassa@myumanitoba.ca>
2024-10-23 22:59:19 -0500

    task done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,11 +5,11 @@ Contains a cache class
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 from functools import wraps
 
 
-def count_calls(method: callable) -> callable:
+def count_calls(method: Callable) -> Callable:
     """count calls function
     this function increments a key of the function name
     using redis everytime the function is called
@@ -23,6 +23,26 @@ def count_calls(method: callable) -> callable:
         """
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """stores the history of inputs and outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """saves the input and output of each function in redis
+        """
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, str(output))
+
+        return output
 
     return wrapper
 
@@ -46,7 +66,7 @@ class Cache:
 
         return (key)
 
-    def get(self, key: str, fn: callable = None):
+    def get(self, key: str, fn: Callable = None):
         """get method"""
         data = self._redis.get(key)
 
