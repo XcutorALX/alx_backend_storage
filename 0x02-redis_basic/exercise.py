f2a75a7979dea473512aac6b5fb03af7c630bba0
UnchainedDev <abassa@myumanitoba.ca>
2024-10-23 23:11:00 -0500

    task done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -85,6 +85,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         key = str(uuid.uuid1())
